ea5aa9917693abbf2a3d6563f321201d7063f755
protocol10 <mukadam.akshay@gmail.com>
2026-04-07 17:14:07 +0530

    feat: added library dependences

--- app/src/main/java/com/akshaym/weatherappcompose/di/NetworkModule.kt
+++ app/src/main/java/com/akshaym/weatherappcompose/di/NetworkModule.kt
@@ -3,24 +3,36 @@ package com.akshaym.weatherappcompose.di
 import com.akshaym.weatherappcompose.network.WeatherApi
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 
 const val WEATHER_BASE_URL = "https://dataservice.accuweather.com/"
 
 @Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
     @Provides
+    @Singleton
     fun providesOkHttp(): OkHttpClient {
         return OkHttpClient.Builder().build()
     }
 
     @Provides
-    fun providesRetrofit(okHttpClient: OkHttpClient) {
-        Retrofit.Builder().baseUrl(WEATHER_BASE_URL).client(okHttpClient)
+    @Singleton
+    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder().baseUrl(WEATHER_BASE_URL).client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create()).build()
-            .create(WeatherApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesWeatherApi(retrofit: Retrofit): WeatherApi {
+        return retrofit.create(WeatherApi::class.java)
     }
 }
